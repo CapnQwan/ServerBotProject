e8b8931a7bb0e48a35afe3242f6d9eaf3f46e73d
CapnQwan <q.p1453@gmail.com>
2024-04-01 18:05:20 +1300

    Added Server Connectivity

--- Main.py
+++ Main.py
@@ -1,27 +1,45 @@
 from tkinter import *
 from tkinter import ttk
+import socket
+import threading
+
+class Server():
+  def __init__(self,Adress=('',5000),MaxClient=1):
+      self.s = socket.socket()
+      self.s.bind(Adress)
+      self.s.listen(MaxClient)
+  def WaitForConnection(self):
+      print('Waiting for connection')
+      self.Client, self.Adr=(self.s.accept())
+      connectionString = 'Got a connection from: '+str(self.Client)+'.'
 
 class App(Tk):
    def __init__(self):
       super().__init__()
 
 WINDOW_TABS = ['Main', 'Stats', 'Debug']
-
+connectionString = 'waiting for connection...'
 
 app = App()
 nb = ttk.Notebook(app)
 
 frame1 = Frame(nb, width=500, height=350)
 
-label1 = ttk.Label(frame1, text = "This is Window One")
+label1 = ttk.Label(frame1, text = connectionString)
 label1.pack(pady = 50, padx = 20)
 
-frame2 = Frame(nb, width=500, height=350)
 frame1.pack()
 
 
-nb.add(frame1, text = "Window 1")
-nb.add(frame2, text = "Window 2")
+nb.add(frame1)
 
 nb.pack(padx = 0, pady = 0, expand = True)
+
+
+s = Server()
+
+thread = threading.Thread(target=s.WaitForConnection)
+thread.daemon = True
+thread.start()
+
 app.mainloop()
