51f36268d5bfb035e40fdf1de0a13bef3fc8b979
CapnQwan <q.p1453@gmail.com>
2024-04-12 07:44:58 +1200

    minor tweak  to openMonopoly

--- InstanceManager/Instance_Handlers/openMonopoly.py
+++ InstanceManager/Instance_Handlers/openMonopoly.py
@@ -2,6 +2,8 @@ import pyautogui
 
 def openMonopoly(instance):
   image_location = pyautogui.locateOnScreen('./Images/MonopolyGoIcon.PNG', confidence=0.8)
+  print('opening monopoly')
   if image_location:
+    print('icon found')
     pyautogui.click(image_location)
     state = 'initilize'
